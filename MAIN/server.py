212337f2ff05aad184a950bc3a0720a1a037b6a8
Preston Brubaker <brubakerpreston@gmail.com>
2023-11-05 19:49:52 -0500

    pigpu

--- MAIN/server.py
+++ MAIN/server.py
@@ -1,11 +1,16 @@
 import http.server
 import socketserver
+import os
 
 PORT = 58541
+
+# Define the directory where your HTML file and assets are located.
+web_dir = os.path.join(os.path.dirname(__file__), 'main.html')
+os.chdir(web_dir)
+
 Handler = http.server.SimpleHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
 print(f"Serving at port {PORT}")
 httpd.serve_forever()
-
