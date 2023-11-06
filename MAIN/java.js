20413fe56573b36d645dd1d1dfa9d8f46e0f9e82
Preston Brubaker <brubakerpreston@gmail.com>
2023-11-05 21:29:34 -0500

    d

--- MAIN/java.js
+++ MAIN/java.js
@@ -1,1 +1,22 @@
+// Function to load chat data and display it
+function loadChat() {
+    fetch('chat_data.json')
+        .then(response => response.json())
+        .then(data => {
+            const chatBox = document.getElementById('chat-box');
+            chatBox.innerHTML = ''; // Clear chat box
+            data.forEach(message => {
+                const p = document.createElement('p');
+                p.textContent = message.chat; // Add message
+                chatBox.appendChild(p);
+            });
+        })
+        .catch(error => console.error('Error fetching chat data:', error));
+}
+
+// Set interval to refresh chat every 5 seconds
+setInterval(loadChat, 5000);
+
+// Load chat when the page loads
+window.onload = loadChat;
 
--- MAIN/server.py
+++ MAIN/server.py
@@ -1,11 +1,43 @@
 import http.server
 import socketserver
+import json
+from urllib.parse import parse_qs
 
 PORT = 58541
-Handler = http.server.SimpleHTTPRequestHandler
+DATA_FILE = 'chat_data.json'
 
-# The Handler will automatically serve files from the current directory.
-httpd = socketserver.TCPServer(("", PORT), Handler)
+class ChatHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        # Check if the path is correct
+        if self.path == '/submit-form':
+            # Get the length of the data and read it
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode('utf-8')
+            chat_data = parse_qs(post_data)['chat'][0]
 
+            # Load existing chat data
+            try:
+                with open(DATA_FILE, 'r') as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                data = []
+
+            # Append new chat message
+            data.append({'chat': chat_data})
+            
+            # Save back to the file
+            with open(DATA_FILE, 'w') as file:
+                json.dump(data, file)
+
+            # Redirect back to the main page
+            self.send_response(303)  # Redirect with See Other response
+            self.send_header('Location', '/')
+            self.end_headers()
+        else:
+            # Default to the superclass method if the path is not the one we handle
+            super().do_POST()
+
+# Initialize and start the server
+httpd = socketserver.TCPServer(("", PORT), ChatHandler)
 print(f"Serving at port {PORT}")
 httpd.serve_forever()
