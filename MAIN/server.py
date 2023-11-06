bb31ca021da1233817bbc82296c4ff5ca6e2bb14
Preston Brubaker <brubakerpreston@gmail.com>
2023-11-05 19:53:04 -0500

    d

--- MAIN/server.py
+++ MAIN/server.py
@@ -1,15 +1,10 @@
 import http.server
 import socketserver
-import os
 
 PORT = 58541
-
-# Define the directory where your HTML file and assets are located.
-web_dir = os.path.join(os.path.dirname(__file__), 'main.html')
-os.chdir(web_dir)
-
 Handler = http.server.SimpleHTTPRequestHandler
 
+# The Handler will automatically serve files from the current directory.
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
 print(f"Serving at port {PORT}")
